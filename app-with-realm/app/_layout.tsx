748505890eade4447af02c0169cc7dd1b837eb2d
Anay Apte <aaapte@stu.naperville203.org>
2024-06-28 09:56:46 -0500

    working on the tutorial for the database

---
+++ app-with-realm/app/_layout.tsx
@@ -0,0 +1,10 @@
+import { Stack } from "expo-router";
+import { RealmProvider } from "@realm/react";
+
+export default function AppLayout() {
+    return(
+      <RealmProvider schema = {}>
+        <Stack />
+      </RealmProvider>  
+    );
+}
---
+++ app-with-realm/app/index.tsx
@@ -0,0 +1,9 @@
+// /app/index.tsx
+import React from 'react';
+import { Text } from 'react-native';
+
+const Page: React.FC = () => {
+    return <Text>Home page</Text>
+};
+
+export default Page;
