4ff039d80ffe4da7d97328ba3018f5eadb25109f
Anay Apte <aaapte@stu.naperville203.org>
2024-08-10 17:31:38 -0500

    development database in progress

--- one_percent_better/app/RecipesPage.jsx
+++ one_percent_better/app/RecipesPage.jsx
@@ -1,10 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, FlatList, Image, StyleSheet, TouchableOpacity } from 'react-native';
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://hhaknhsygdajhabbanzu.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhoYWtuaHN5Z2RhamhhYmJhbnp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjAwMjQ3MjEsImV4cCI6MjAzNTYwMDcyMX0.kK8viaMqxFPqylFTr0RvC0V6BL6CtB2jLgZdn-AhGc4'
-const supabase = createClient(supabaseUrl, supabaseKey)
+import { supabase } from '../utils/supabaseClient';
 
 const RecipesPage = () => {
   const [recipes, setRecipes] = useState([]);
