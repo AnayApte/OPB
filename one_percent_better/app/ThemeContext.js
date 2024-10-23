232c7d132a4e8446bcbb4fd36717e9289cd8d219
sryerabati <sryerabati@stu.naperville203.org>
2024-10-23 11:11:01 -0500

    fuka nay anpte

--- one_percent_better/app/ThemeContext.js
+++ one_percent_better/app/ThemeContext.js
@@ -8,8 +8,8 @@ export const useTheme = () => useContext(ThemeContext);
 
 export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState({
-    background: '#3b0051',
-    text: '#f4e4fc',
+    background: '#ffffffff',
+    text: '#000000',
     primary: '#641f1f',
     secondary: '#f2f5ea',
     tertiary: '#a69998'
