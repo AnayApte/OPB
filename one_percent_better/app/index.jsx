52f93500196b2fbbef9beb6b95d1dcccd6bfcaaf
sryerabati <sryerabati@stu.naperville203.org>
2024-10-23 14:57:30 -0500

    PULL THIS STUFF MANE

--- one_percent_better/app/index.jsx
+++ one_percent_better/app/index.jsx
@@ -2,7 +2,7 @@ import { Redirect } from 'expo-router';
 import { LogBox } from 'react-native';
 
 // Hide specific warnings (if you want to ignore all, just use ignoreAllLogs)
-LogBox.ignoreLogs();
+LogBox.ignoreLogs(['Warning: ...']);
 
 // Or you can hide all logs (warnings, errors, etc.):
 // LogBox.ignoreAllLogs(); // Uncomment this to hide all warnings and errors
